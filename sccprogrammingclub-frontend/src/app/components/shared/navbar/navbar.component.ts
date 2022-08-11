58fa62a6c96c5cc2ee23d2ac8ccf89a403e67b78
franfonse <franfonseca8594@gmail.com>
2022-08-11 02:35:30 -0700

    added responsive and cool menu dropdown

--- sccprogrammingclub-frontend/src/app/components/shared/navbar/navbar.component.ts
+++ sccprogrammingclub-frontend/src/app/components/shared/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   screenWidth: any;
+  smallScreen: boolean = true;
   collapsed: boolean = true;
 
   constructor() {
@@ -20,13 +21,23 @@ export class NavbarComponent implements OnInit {
   @HostListener('window:resize', [])
   private onResize() {
     this.screenWidth = window.innerWidth;
-  }
-
-  show() {
-    this.collapsed = false;
+    if (this.screenWidth < 590) {
+      this.smallScreen = true;
+    } else {
+      this.smallScreen = false;
+    }
   }
 
   hide() {
     this.collapsed = true;
+  }
+
+  menu() {
+    if (this.collapsed) {
+      this.collapsed = false;
+    } else {
+      this.collapsed = true;
+    }
+    return this.collapsed;
   }
 }
