a22d3c2edde7fc6f4812ef347bc7b5a023c95cfe
franfonse <franfonseca8594@gmail.com>
2022-08-19 23:45:43 -0700

    members JSON

--- sccprogrammingclub-frontend/src/app/pages/current-members/current-members.component.ts
+++ sccprogrammingclub-frontend/src/app/pages/current-members/current-members.component.ts
@@ -10,31 +10,10 @@ import { MemberService } from 'src/app/services/member/member.service';
 export class CurrentMembersComponent implements OnInit {
 
   currentYear: string = '2021-2022';
-  currentMembers: Member[] = [{
-    name: 'Fran',
-    username: 'francisco-f',
-    title: 'President',
-    desc: 'The code is great.',
-    bio: 'Fran\'s bioooo.',
-    img: 'https://media.istockphoto.com/vectors/cute-kawaii-beautiful-face-smile-and-happy-vector-id695343600'
-  }, {
-    name: 'Dan',
-    username: 'daniel-j',
-    title: 'VP',
-    desc: 'Coding is so much fun',
-    bio: 'I just love to code.',
-    img: 'https://media.istockphoto.com/vectors/cute-kawaii-beautiful-face-smile-and-happy-vector-id695343600'
-  },
-  {
-    name: 'Sara',
-    username: 'sara-l',
-    title: 'Member',
-    desc: 'My name is Sara! :D',
-    bio: 'My life is so adventurous!',
-    img: 'https://media.istockphoto.com/vectors/cute-kawaii-beautiful-face-smile-and-happy-vector-id695343600'
-  }];
+  currentMembers: Member[] = [];
 
-  constructor() {
+  constructor(private memberService: MemberService) {
+    this.currentMembers = memberService.getCurrentMembers();
   }
 
   ngOnInit(): void {
--- sccprogrammingclub-frontend/src/app/pages/member/member.component.ts
+++ sccprogrammingclub-frontend/src/app/pages/member/member.component.ts
@@ -9,16 +9,10 @@ import { Member } from 'src/app/interfaces/member.interface';
 export class MemberComponent implements OnInit {
 
   @Input()
-  member: Member = {
-    name: 'Fran',
-    username: 'francisco-f',
-    title: 'President',
-    desc: 'The code is great.',
-    bio: 'Fran\'s bioooo.',
-    img: 'https://media.istockphoto.com/vectors/cute-kawaii-beautiful-face-smile-and-happy-vector-id695343600'
-  };
+  member?: Member;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
