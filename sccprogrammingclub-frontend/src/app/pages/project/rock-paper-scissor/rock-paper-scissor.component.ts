48c8492b857cc0f684d6bbf80ef3fadb95bc3b3c
Francisco Fonseca <franfonseca8594@gmail.com>
2023-04-27 14:54:05 -0700

    added features to rsp

--- sccprogrammingclub-frontend/src/app/pages/project/rock-paper-scissor/rock-paper-scissor.component.ts
+++ sccprogrammingclub-frontend/src/app/pages/project/rock-paper-scissor/rock-paper-scissor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { _countGroupLabelsBeforeOption } from '@angular/material/core';
 
 @Component({
   selector: 'app-rock-paper-scissor',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./rock-paper-scissor.component.css']
 })
 export class RockPaperScissorComponent {
+  
+  scoreUser:number = 0;
+  scoreComp:number = 0;
+
+  choices:string[] = ["rock", "paper", "scissors"];
+  compChoice:string = "";
+
+start() {
+
+}
+
+  selectItem(choice:string) {
+    switch(choice) {
+      case 'rock':
+
+      break;
+      case 'paper':
+
+      break;
+      case 'scissors':
+
+      break;
+    }
+  }
+
+  nextRound() {
+    
+  }
+
+  end() {
+
+  }
 
 }
